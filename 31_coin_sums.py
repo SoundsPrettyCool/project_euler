1be457b49c23bb58274751799838ccb9f7f8bc8a
SoundsPrettyCool <146213634+SoundsPrettyCool@users.noreply.github.com>
2024-11-10 04:07:30 +0000

    finish problem 31 in project euler

--- 31_coin_sums.py
+++ 31_coin_sums.py
@@ -1,22 +1,22 @@
 # 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
 
-def coin_sums(target, memo={}):
-    if target in memo:
-        return memo[target]
+def coin_sums(target, coins, start_idx, memo= {}):
+    if (target,start_idx) in memo:
+        return memo[(target, start_idx)]
     if target == 0:
         return 1
     elif target < 0:
         return 0
 
     sum = 0
-    coins = [1,2,5,10,20,50,100,200]
 
-    for coin in coins:
-        sum += coin_sums(target-coin, memo)
+    for i in range(start_idx, len(coins)):
+        sum += coin_sums(target-coins[i], coins, i, memo)
 
-    if target not in memo:
-        memo[target] = sum
+    if (target,start_idx) not in memo:
+        memo[(target,start_idx)] = sum
 
     return sum
 
-print(coin_sums(2))
+coins = [1,2,5,10,20,50,100,200]
+print(coin_sums(200, coins, 0))
